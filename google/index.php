aff282bcb548a3dc261be594957a860f5c067941
aachalshres <aachalshrsth@gmail.com>
2025-01-06 07:16:18 +0545

    Update index.php

--- google/index.php
+++ google/index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Google</h1>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Google</title>
+    </head>
+    <body>
+    <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_120x44dp.png" alt="logo">
+    <form action="https://google.com/search">
+        <input type="text">
+    </form>
+    </body>
 </html>
