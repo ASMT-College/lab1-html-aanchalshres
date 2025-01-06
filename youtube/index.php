20827a5b428fa20532752704061471770df911d5
aachalshres <aachalshrsth@gmail.com>
2025-01-06 06:50:12 +0545

    new folder

--- youtube/index.php
+++ youtube/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hello World!</title>
 </head>
 <body>
     <h1>Hi</h1>
@@ -13,8 +13,8 @@
     ?>
     <br>
     <script>
-        document.write('Hello from client'+new Date())
+        document.write('Hello from client '+new Date())
         alert('hi')
         </script>
 </body>
-</html>
+</html> 
