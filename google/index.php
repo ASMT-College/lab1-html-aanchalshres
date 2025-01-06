9ba4c82c102cdf5d0af634fa4e13646755b0e08b
aachalshres <aachalshrsth@gmail.com>
2025-01-06 18:28:44 +0545

    google

--- google/index.php
+++ google/index.php
@@ -4,11 +4,20 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Google</title>
+        <link rel="icon" href="img/gg.png" type="image/icon type">
+        <link style = "text/css" rel="stylesheet" href="style.css">
     </head>
     <body>
+        
+        <div class="search-container">
     <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_120x44dp.png" alt="logo">
-    <form action="https://google.com/search">
-        <input type="text">
-    </form>
+    <form action="https://google.com/search" method="get">
+        <input type="text" name="q" class="search-input" placeholder="Search...">
+        <button type="submit" class="search-button">Search</button>
+        <br><br>
+        <button>Google Search</button>
+        <button>I'm feeling lucky</button>
+        </form>
+    </div>
     </body>
 </html>
